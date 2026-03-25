6b1ee959040e5438bf951f8c99df0344e3b766c1
intergrav <devin@devins.page>
2026-03-25 03:59:07 -0400

    feat: rewrite packversion.kt and support new versioning
    supports all adrenaline/additive versioning schemes, including the new upcoming one with the `mc` tag to prevent confusion between modpack version and minecraft ver

--- src/main/kotlin/io/github/gaming32/additiveinstaller/PackVersion.kt
+++ src/main/kotlin/io/github/gaming32/additiveinstaller/PackVersion.kt
@@ -11,21 +11,30 @@ class PackVersion(val modpack: Modpack, val data: JsonObject) {
     val datePublished: Instant
 
     init {
-        val versionNumber = data["version_number"].asString
-        loader = if ('-' in versionNumber && '+' !in versionNumber) {
-            packVersion = versionNumber.substringBefore('-')
-            gameVersion = versionNumber.substringAfterLast('-')
-            if (versionNumber.count { it == '-' } > 1) {
-                versionNumber.substringAfter('-').substringBeforeLast('-')
-            } else {
-                "FABRIC"
-            }
-        } else {
-            packVersion = versionNumber.substringBefore('+')
-            gameVersion = versionNumber.substringAfter('+').substringBeforeLast('.')
-            versionNumber.substringAfterLast('.')
-        }.uppercase().let(Loader::valueOf)
+        val (packVer, gameVer, ldr) = parseVersion(data["version_number"].asString)
+        packVersion = packVer
+        gameVersion = gameVer
+        loader = ldr
         datePublished = Instant.parse(data["date_published"].asString)
+    }
+
+    private fun parseVersion(versionNumber: String): Triple<String, String, Loader> = when {
+        '+' in versionNumber -> versionNumber.substringAfter('+').removePrefix("mc").let {
+            Triple(
+                versionNumber.substringBefore('+'),
+                it.substringBeforeLast('.'),
+                Loader.valueOf(it.substringAfterLast('.').uppercase())
+            )
+        }
+        '-' in versionNumber -> {
+            val parts = versionNumber.split('-')
+            when (parts.size) {
+                2 -> Triple(parts[0], parts[1], Loader.FABRIC)
+                3 -> Triple(parts[0], parts[2], Loader.valueOf(parts[1].uppercase()))
+                else -> Triple(versionNumber, versionNumber, Loader.FABRIC)
+            }
+        }
+        else -> Triple(versionNumber, versionNumber, Loader.FABRIC)
     }
 
     val launcherFolderPath = "${modpack.id}/$packVersion-$gameVersion-$loader"
